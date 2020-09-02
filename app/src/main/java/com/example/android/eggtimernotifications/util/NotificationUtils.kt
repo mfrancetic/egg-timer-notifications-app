0e7fe1f575d2fe746f273466fab0fe76310e65a6
Maja Francetic <francetic.maja@gmail.com>
2020-09-02 14:45:39 +0200

    Create an extension function NotificationManager.cancelNotifications() and call it in the startTimer method in the EggTimerViewModel

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -75,4 +75,9 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     notify(NOTIFICATION_ID, builder.build())
 }
 
-// TODO: Step 1.14 Cancel all notifications
+/**
+ * Cancels all notifications
+ */
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
